d27401c7a8e97c4259dfc4670575d23b4eec67ae
Irtesam Virani <59720745+Samcanes@users.noreply.github.com>
2021-11-30 14:32:13 +0530

    Initiate

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Layout from "./Layout";
+
+// import { render } from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout></Layout>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Avatar/index.js
@@ -0,0 +1,19 @@
+import "./index.css";
+import avatar from "./../../public/images/avatar.png";
+
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+export default function Avatar() {
+  return (
+    <>
+      <div>
+        <Row className="center">
+          <Col className="vertical-center">
+            <img id="avtar-img" src={avatar} alt="avatar pic" />
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
